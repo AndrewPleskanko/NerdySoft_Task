caa6ab7c6209876cf6ae4ac56c45735bcef32421
AndrewPleskanko <pleskankoandrew@gmail.com>
2024-08-23 23:31:07 +0300

    Fix version in controller and fix openApi

--- src/main/java/org/example/nerdysoft/NerdysoftApplication.java
+++ src/main/java/org/example/nerdysoft/NerdysoftApplication.java
@@ -9,5 +9,5 @@ public class NerdysoftApplication {
     public static void main(String[] args) {
         SpringApplication.run(NerdysoftApplication.class, args);
     }
-
 }
+
--- src/main/java/org/example/nerdysoft/input/web/BookController.java
+++ src/main/java/org/example/nerdysoft/input/web/BookController.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/api/books/v1")
 @RequiredArgsConstructor
 public class BookController {
 
